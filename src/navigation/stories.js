93646c19c25a3f30c0691860f0d3ca641c0d6e51
Matt Travi <programmer@travi.org>
2017-11-01 22:48:50 -0500

    wip(pagination): initial version of the router implementation

--- src/navigation/stories.js
+++ src/navigation/stories.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {linkTo} from '@storybook/addon-links';
+import {Link} from 'react-router';
+import storyRouter from 'storybook-router';
 
 storiesOf('Navigation/Pagination/linkTo', module)
   .add('first page', () => (
@@ -18,5 +20,28 @@ storiesOf('Navigation/Pagination/linkTo', module)
   .add('last page', () => (
     <ol>
       <li><button onClick={linkTo('Navigation/Pagination/linkTo', 'middle page')}>Previous</button></li>
+    </ol>
+  ));
+
+storiesOf('Navigation/Pagination/router', module)
+  .addDecorator(storyRouter({
+    '/first': linkTo('Navigation/Pagination/router', 'first page'),
+    '/middle': linkTo('Navigation/Pagination/router', 'middle page'),
+    '/last': linkTo('Navigation/Pagination/router', 'last page')
+  }))
+  .add('first page', () => (
+    <ol>
+      <li><Link to="/middle">Next</Link></li>
+    </ol>
+  ))
+  .add('middle page', () => (
+    <ol>
+      <li><Link to="/first">Previous</Link></li>
+      <li><Link to="/last">Next</Link></li>
+    </ol>
+  ))
+  .add('last page', () => (
+    <ol>
+      <li><Link to="/middle">Previous</Link></li>
     </ol>
   ));
