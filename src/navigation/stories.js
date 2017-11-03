896a5d678fac1ddf2ca8132870b35df2faf88108
Matt Travi <programmer@travi.org>
2017-11-03 09:51:16 -0500

    style(pagination): aligned the navigation horizontally

--- src/navigation/stories.js
+++ src/navigation/stories.js
@@ -5,21 +5,25 @@ import {linkTo} from '@storybook/addon-links';
 import {Link} from 'react-router';
 import storyRouter from 'storybook-router';
 
+const paginationContainerStyles = {display: 'flex', justifyContent: 'space-between', padding: 0, listStyleType: 'none'};
+
 storiesOf('Navigation/Pagination/linkTo', module)
   .add('first page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
+      <li>&nbsp;</li>
       <li><button onClick={linkTo('Navigation/Pagination/linkTo', 'middle page')}>Next</button></li>
     </ol>
   ))
   .add('middle page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
       <li><button onClick={linkTo('Navigation/Pagination/linkTo', 'first page')}>Previous</button></li>
       <li><button onClick={linkTo('Navigation/Pagination/linkTo', 'last page')}>Next</button></li>
     </ol>
   ))
   .add('last page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
       <li><button onClick={linkTo('Navigation/Pagination/linkTo', 'middle page')}>Previous</button></li>
+      <li>&nbsp;</li>
     </ol>
   ));
 
@@ -30,18 +34,20 @@ storiesOf('Navigation/Pagination/router', module)
     '/last': linkTo('Navigation/Pagination/router', 'last page')
   }))
   .add('first page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
+      <li>&nbsp;</li>
       <li><Link to="/middle">Next</Link></li>
     </ol>
   ))
   .add('middle page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
       <li><Link to="/first">Previous</Link></li>
       <li><Link to="/last">Next</Link></li>
     </ol>
   ))
   .add('last page', () => (
-    <ol>
+    <ol style={paginationContainerStyles}>
       <li><Link to="/middle">Previous</Link></li>
+      <li>&nbsp;</li>
     </ol>
   ));
