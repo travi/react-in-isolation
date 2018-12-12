52f73518c04130d5e02a8ed1dd407828ec3066a9
Matt Travi <programmer@travi.org>
2018-12-11 23:34:22 -0600

    feat(addon-info): configured the info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-import {configure} from '@storybook/react';
+import {configure, addDecorator} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
 
 function loadStories() {
   const req = require.context('../src', true, /.stories.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
+addDecorator(withInfo);
 configure(loadStories, module);
